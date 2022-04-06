5e4af36ca6183a5485fe52cb3b31e4a6b6812afe
叶俊星(达诺) <junxing.yjx@alibaba-inc.com>
2022-04-06 17:13:59 +0800

    feat: support check-all buttons.

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ let lastNodesContent = '';
 let nodeSelectorContainerElem = document.getElementById('node-selector-container')
 let selectedNodeStrElem = document.getElementById('selected-node-str')
 
+function encode(str) {
+  const en = {
+    '"': '&quot;',
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    ' ': '&nbsp;',
+  }
+
+  let rst = '';
+  for (let i = 0; i < str.length; i++) {
+    rst = rst + (en[str[i]] || str[i]);
+  }
+  return rst;
+}
+
 function setNodes(nodes) {
   nodeSelectorContainerElem.innerHTML = nodes.map(node => {
     const id = md5(node.src)
     return `
   <div class="form-check">
-    <input class="form-check-input" type="checkbox" data-nodeId="${node.nodeId}" id="${id}">
+    <input class="form-check-input" type="checkbox" data-nodeId="${encode(node.nodeId)}" id="${id}">
     <label class="form-check-label" style="white-space: nowrap" for="${id}">
       ${node.src}
     </label>
@@ -17,7 +33,9 @@ function setNodes(nodes) {
 }
 
 function parseNodes(content) {
-  const splited = content.split('\n').slice(1).map(s => s.trim());
+  const splited = content.split('\n')
+                    .filter(s => !s.includes('靠前节点较为拥挤'))
+                    .map(s => s.trim());
 
   return splited.map(s => ({
     src: s,
@@ -43,6 +61,22 @@ function restoreCopyBtn() {
   elem.classList.add('btn-success')
 }
 
+function checkAll(state) {
+
+  return () => {
+    const elems = Array.from(document.querySelectorAll('#node-selector-container .form-check-input'))
+    for (let i = 0; i < elems.length; i++) {
+      const elem = elems[i];
+      if (state === 'inverse') {
+        elem.checked = !elem.checked;
+      } else {
+        elem.checked = state;
+      }
+    };
+    refreshSelected();
+  }
+}
+
 function copySelected(e) {
   navigator.clipboard.writeText(selectedNodeStrElem.value)
   e.target.innerHTML = '已复制！'
@@ -51,25 +85,29 @@ function copySelected(e) {
   setTimeout(() => restoreCopyBtn(), 2000)
 }
 
-function onNodesContentChanged(e) {
+function onNodesContentChanged() {
   const newContent = document.getElementById('nodes-content').value;
   if (lastNodesContent === newContent) { return; }
   const nodes = parseNodes(newContent);
   setNodes(nodes)
   document.getElementById('copy-group').style.visibility = 'visible';
+  document.getElementById('check-controller-container').style.visibility = 'visible';
 }
 
 async function pasteNodesStr() {
   const str = await navigator.clipboard.readText();
   document.getElementById('nodes-content').value = str;
+  document.getElementById('load').click();
 }
 
 function initEvents() {
   document.getElementById('load').addEventListener('click', onNodesContentChanged)
   document.getElementById('copy-selected').addEventListener('click', copySelected)
+  document.getElementById('check-all').addEventListener('click', checkAll(true))
+  document.getElementById('check-none').addEventListener('click', checkAll(false))
+  document.getElementById('check-inverse').addEventListener('click', checkAll('inverse'))
   document.getElementById('paste-nodes-str').addEventListener('click', pasteNodesStr)
   nodeSelectorContainerElem.addEventListener('click', refreshSelected)
-
 }
 
 window.onload = async () => {
